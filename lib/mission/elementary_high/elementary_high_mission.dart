4b901f6e4fa252fdcbc851d8f8df16cc86cbc8ce
2chung1 <cw3714@naver.com>
2025-08-18 15:08:24 +0900

    trouble_shooting

--- lib/mission/elementary_high/elementary_high_mission.dart
+++ lib/mission/elementary_high/elementary_high_mission.dart
@@ -416,7 +416,6 @@ class _ElementaryHighMissionScreenState
                       width: double.infinity,
                       fit: BoxFit.cover,
                     ),
-                    const SizedBox(height: 24),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
@@ -465,10 +464,12 @@ class _ElementaryHighMissionScreenState
                                       color: const Color(0xffdcdcdc)),
                                 ),
                                 child: TextField(
+                                  style: const TextStyle(fontSize:14),
                                   controller: _answerController,
                                   decoration: InputDecoration(
                                     hintText: '정답을 입력해 주세요.',
                                     hintStyle: const TextStyle(
+                                        fontSize: 14,
                                         color: Color(0xffaaaaaa)),
                                     contentPadding: const EdgeInsets
                                         .symmetric(
