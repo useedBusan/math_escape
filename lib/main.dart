69ac212bce7d8cee24c02f438f204b1802b988a1
palrang22 <kseunghui@gmail.com>
2025-11-21 15:49:48 +0900

    [feat] implement app lifecycle observer for audio control

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,47 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.paused:
+      // iOS와 Android 모두 백그라운드 진입 시
+        serviceLocator.audioService.goToBackground();
+        break;
+      case AppLifecycleState.resumed:
+      // 다시 포그라운드로 돌아올 때
+        serviceLocator.audioService.backToApp();
+        break;
+      case AppLifecycleState.inactive:
+      // Android에서 전환 중일 때도 일시정지하려면 여기 추가
+        break;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
